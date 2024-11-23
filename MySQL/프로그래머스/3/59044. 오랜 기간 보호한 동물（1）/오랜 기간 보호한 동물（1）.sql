8c061ebba9ab48a698055a4963d94d395856be2e
Dee H Seon <tunacome@gmail.com>
2024-11-23 09:13:00 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+select
+    ins.name,
+    ins.datetime
+from
+    animal_ins ins
+    left join animal_outs outs
+    on ins.animal_id = outs.animal_id
+where outs.datetime is null
+order by ins.datetime asc
+limit 3

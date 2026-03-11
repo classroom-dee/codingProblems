d2c0fd61b5618b52efd56095f18cf2fabb7eed06
Dee <123602432+boolYikes@users.noreply.github.com>
2026-03-12 03:53:56 +0900

    0067-ADD-BINARY Stats: Time: 0 ms (100%), Space: 2.1 MB (64.18%) - LeetHub

---
+++ Leet/0067-add-binary/0067-add-binary.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn add_binary(a: String, b: String) -> String {
+        let mut i = a.len() as i32 - 1;
+        let mut j = b.len() as i32 - 1;
+        let mut carry = 0;
+        let mut result = String::new();
+
+        let a_bytes = a.as_bytes();
+        let b_bytes = b.as_bytes();
+
+        while i >= 0 || j >= 0 || carry > 0 {
+            let mut sum = carry;
+
+            if i >= 0 {
+                sum += (a_bytes[i as usize] - b'0') as i32;
+                i -= 1;
+            }
+
+            if j >= 0 {
+                sum += (b_bytes[j as usize] - b'0') as i32;
+                j -= 1;
+            }
+
+            result.insert(0, char::from(b'0' + (sum % 2) as u8));
+            carry = sum / 2;
+        }
+
+        result
+    }
+}
